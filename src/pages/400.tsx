bb1777e534a87c76e25c32fe3e3d97cccb56a9cf
David Bowland <dbowland1@gmail.com>
2022-04-30 22:51:24 -0500

    Switch to wrapPageElement

--- src/pages/400.tsx
+++ src/pages/400.tsx
@@ -1,20 +1,16 @@
-import Paper from '@mui/material/Paper'
 import React from 'react'
 
-import '@config/amplify'
+import Authenticated from '@components/auth'
 import ServerErrorMessage from '@components/server-error-message'
-import Themed from '@components/themed'
 
 const BadRequest = (): JSX.Element => {
   return (
-    <Themed>
-      <Paper elevation={3} sx={{ margin: '1em auto', maxWidth: '900px' }}>
-        <ServerErrorMessage title="400: Bad Request">
-          Your request was malformed or otherwise could not be understood by the server. Please modify your request
-          before retrying.
-        </ServerErrorMessage>
-      </Paper>
-    </Themed>
+    <Authenticated initialAuthState="signIn" initialShowLogin={false}>
+      <ServerErrorMessage title="400: Bad Request">
+        Your request was malformed or otherwise could not be understood by the server. Please modify your request before
+        retrying.
+      </ServerErrorMessage>
+    </Authenticated>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,32 @@
 import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
-import Paper from '@mui/material/Paper'
 
-import '@config/amplify'
 import { AuthState } from '@types'
 import Authenticated from '@components/auth'
 import SessionCreate from '@components/session-create'
-import Themed from '@components/themed'
 
 const Index = (): JSX.Element => {
   const [authState, setAuthState] = useState<AuthState>('signIn')
   const [showLogin, setShowLogin] = useState(false)
 
   return (
-    <Themed>
+    <>
       <Helmet>
         <title>Choosee | dbowland.com</title>
       </Helmet>
-      <Paper elevation={3} sx={{ margin: 'auto', maxWidth: '900px' }}>
-        <Authenticated
-          initialAuthState={authState}
-          initialShowLogin={showLogin}
-          setInitialAuthState={setAuthState}
-          setInitialShowLogin={setShowLogin}
-        >
-          <main className="main-content" style={{ minHeight: '90vh' }}>
-            <section>
-              <SessionCreate setAuthState={setAuthState} setShowLogin={setShowLogin} />
-            </section>
-          </main>
-        </Authenticated>
-      </Paper>
-    </Themed>
+      <Authenticated
+        initialAuthState={authState}
+        initialShowLogin={showLogin}
+        setInitialAuthState={setAuthState}
+        setInitialShowLogin={setShowLogin}
+      >
+        <main className="main-content" style={{ minHeight: '90vh' }}>
+          <section>
+            <SessionCreate setAuthState={setAuthState} setShowLogin={setShowLogin} />
+          </section>
+        </main>
+      </Authenticated>
+    </>
   )
 }
 
